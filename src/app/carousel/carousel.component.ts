265bd2c42358ee3fd959507c21ef2cc49c9901a9
Lucas Spirito <lucas.terrand@viajobien.com>
2020-07-18 11:39:20 -0300

    First Commit with Angular project structure

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
+})
+export class CarouselComponent implements OnInit {
+  constructor() {}
+  images = [
+    '../../assets/images/broidery_1.jpg',
+    '../../assets/images/broidery_2.jpeg',
+    '../../assets/images/broidery_3.jpeg',
+  ];
+
+  ngOnInit() {}
+}
